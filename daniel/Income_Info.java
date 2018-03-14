e6900075847ea9cb2d0a22b928ae5403787d8e42
janissary1 <bellissimo38@gmail.com>
2018-03-14 16:21:31 -0400

    Income_Info.java

--- daniel/Income_Info.java
+++ daniel/Income_Info.java
@@ -2,13 +2,28 @@ import java.util.*;
 //Need to calculate amount of money coming in from members
 //Need to calculate the amount of money owed to coaches/hall expenses
 public class Income_Info {
+    
     public static void main(String[] args){
         String[][] kek = new String[][]{{"Name","1"},{"Name2","0"}};
         for (String[] i : kek){
             
         }
     }
+    /*
+    Takes an arraylist containing arrays of 2 strings and generates a .txt file representing
+    the profit statement for the month.
+    -Possibly include outstanding charges
+    -Seperates *actual profit* from *estimated profit* depending on who has paid or not
+
+    ArrayList<String[]> -> null 
+    */
     public static void calculateProfit(ArrayList<String[]> in){
+      //Hall rent is 1500$
+      //Coach cost is 500$
+      //Other costs??
       
+
     }
+
+    
 }
