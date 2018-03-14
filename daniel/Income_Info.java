0e5cafa6f7c6ead62d41006bf0ee9bd07530c6bd
janissary1 <bellissimo38@gmail.com>
2018-03-13 22:30:03 -0400

    Income_Info.java

---
+++ daniel/Income_Info.java
@@ -0,0 +1,14 @@
+import java.util.*;
+//Need to calculate amount of money coming in from members
+//Need to calculate the amount of money owed to coaches/hall expenses
+public class Income_Info {
+    public static void main(String[] args){
+        String[][] kek = new String[][]{{"Name","1"},{"Name2","0"}};
+        for (String[] i : kek){
+            
+        }
+    }
+    public static void calculateProfit(ArrayList<String[]> in){
+      
+    }
+}
