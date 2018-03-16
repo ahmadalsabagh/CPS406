13290678b49a64e7884688b549284707c544ffaf
janissary1 <bellissimo38@gmail.com>
2018-03-16 13:40:15 -0400

    Added profitInvoice method, made treasury instance variable private, made paidAccounts method private
    :i

--- Z/Treasurer.java
+++ Z/Treasurer.java
@@ -5,43 +5,64 @@ public class Treasurer {
 	
 	public MessageSystem notifications;
 	private int paidUp;
-	double treasury;
+	private double treasury;
 	
 	public Treasurer(double treasury){
 	    this.notifications = new MessageSystem();
 	    this.treasury = treasury;
+		this.paidAccounts();
 	  }
 	
 	//returns the balance of the treasury
 	public double viewTreasury() {
-		return treasury;
+		return this.treasury;
 	}
 	
 	//adds a specified amount to the treasury
 	public void addToTreasury(double amount) {
 		
-		treasury = treasury + amount;
+		this.treasury += amount;
 		
 	}
 	
 	//sets the treasury to a specified amount
 		public void setTreasury(double amount) {
 			
-			treasury = amount;
+			this.treasury = amount;
 			
 		}
 	//Cycles through the array of members in a given session and returns the number of members who have paid for that session
-		public int paidAccounts(Session current) {
+		private void paidAccounts(Session current) {
 			
 			String[][] temp = current.GetMemberReportInfo();
-			
 			for (int i = 0; i < temp.length; i++)
 	        {
 	            if (temp[i][2] == "true")
 	            {
-	            	paidUp++;
+	            	this.paidUp++;
 	            }
 	        }	
-			return paidUp;
+		}
+		public int getPaid(){
+			return this.paidUp;
+		}
+	//Returns a String value containing information pertaining to club revenue, expenses, and profit for the session.
+		public String profitInvoice(){
+			String[][] temp = current.GetMemberReportInfo();
+			final int hall_pay = 1500;
+			final int coach_pay = 500;
+			//Other expenses?
+			
+			int num_members = temp.length;
+			int notPaid = num_members - this.paidUp;
+			String invoice = 
+			"+-----Monthly Revenue-----+
+			\nMembers: " + num_members + 
+			"\nPaid Members:  " + this.paidUp + 
+			"\nUnpaid Members: " + notPaid +
+			"\n\nClub Revenue: +" + this.paidUp * 200 + "$"
+			"\nClub Expenses: -" + hall_pay + "$ Location rent" +
+			"\n               -" + coach_py + "$ Coach payment";
+
 		}
 }
