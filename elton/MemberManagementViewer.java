2f8a3db1f6188384bfbaa2c30c6308077c2158b6
elton-emmanuel <33668494+elton-emmanuel@users.noreply.github.com>
2018-03-20 12:20:30 -0400

    Login UI is implemented

---
+++ elton/MemberManagementViewer.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author elton
+ */
+public class MemberManagementViewer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Login dog = new Login();
+        dog.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        dog.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
